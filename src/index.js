2ed99d555e91edc1352e98e8822768ecd6aa78ff
Mahmood Al-Malki <mralmalk@iu.edu>
2024-02-15 15:55:31 -0500

    Updated the Arithmetic Service

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { add } = require("./arithmetica");
 
 const app = express();
 
@@ -7,12 +8,15 @@ app.use(cors());
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Arithmetic service - Hello World!');
+app.get("/", (req, res) => {
+  res.send("Arithmetic service - last updated 2/15/2024 3:36 PM");
 });
 
-app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) + Number(req.params.m));
+app.get("/add/:n/:m", (req, res) => {
+  let n = Number(req.params.n);
+  let m = Number(req.params.m);
+  let sum = add(n, m);
+  res.json(sum);
 });
 
 app.listen(port);
